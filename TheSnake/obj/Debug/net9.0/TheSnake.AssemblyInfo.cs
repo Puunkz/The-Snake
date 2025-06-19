826c165871fe9f1503d9da8d5bd6c590d23dbe1a
Puunkz <puunkz1403@gmail.com>
2025-06-19 03:34:22 +0200

    ajout du .gitignore

--- TheSnake/obj/Debug/net9.0/TheSnake.AssemblyInfo.cs
+++ TheSnake/obj/Debug/net9.0/TheSnake.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheSnake")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91331050ad91becdabc887ca25ca32935705e356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a40f87fd97b045761427cba69c760d3a9de71903")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheSnake")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheSnake")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
